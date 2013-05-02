25ae33735bc7d7e17f1e1f311c04b083239ecae6
cubeberg <cubeberg@gmail.com>
2013-05-01 20:45:00 -0400

    Initial project setup

---
+++ ENC28J60_A1/Automation.c
@@ -0,0 +1,12 @@
+/*
+ * Automation.c
+ *
+ *  Created on: May 1, 2013
+ *      Author: cberg
+ */
+
+
+
+
+volatile char autOutputVal = 0;
+volatile char autInputVal = 0;
---
+++ ENC28J60_A1/Automation.h
@@ -0,0 +1,15 @@
+/*
+ * Automation.h
+ *
+ *  Created on: May 1, 2013
+ *      Author: cberg
+ */
+
+#ifndef AUTOMATION_H_
+#define AUTOMATION_H_
+
+extern volatile char autOutputVal;
+extern volatile char autInputVal;
+
+
+#endif /* AUTOMATION_H_ */
